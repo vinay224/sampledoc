878b6da6f0b3dbbccbbdfea8da7922ecc5c3c7ee
vinay <kvkvinay224@gmail.com>
2018-12-19 00:07:41 +0530

    First checkin

---
+++ Add.py
@@ -0,0 +1,17 @@
+# Add of two numbers
+import sys
+def Adding_two(x,y):
+    return 
+try:
+    x = input('enter A value:= ')
+    y = input('enter B value:= ')
+    newvalue = int(x + y)
+    print newvalue
+
+except Exception  :
+    print("entered value is not an Integer")
+    sys.exit()
+    
+
+
+    
